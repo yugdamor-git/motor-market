3a31cfa9e2e24dc6cf2f6b08149f7f8b23457cf7
Yug Damor <yugdamor.dev@gmail.com>
2022-04-20 16:16:32 +0530

    kkk

---
+++ mm/services/api/src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask,jsonify,request
+import os
+
+from autotrader import autotrader
+
+app = Flask(__name__)
+
+app.register_blueprint(autotrader, url_prefix='/autotrader')
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
