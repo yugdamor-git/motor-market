cbc9ad7eefd5aff12aa20397c60c73ba7f13817b
Yug Damor <yugdamor.dev@gmail.com>
2022-04-21 11:59:45 +0530

    k

--- mm/services/machine-learning/numberplate-prediction/handler.py
+++ mm/services/machine-learning/numberplate-prediction/handler.py
@@ -1,46 +1,13 @@
 from pathlib import Path
-from Database  import Database
 from predictor import Predictor
 from PlateRecognizer import PlateRecognizer
 
 
 class Handler:
     def __init__(self) -> None:
-        self.database = Database()
         self.predictor = Predictor()
         self.pathPrefix = Path('/usr/src/app/files')
         self.PlateRecognizer = PlateRecognizer()
-        
-    def test(self):
-        self.database.connect()
-        try:
-            listings = self.database.recUpdate("AT_urls",{"number_plate_flag":2},{"url":"https://www.autotrader.co.uk/car-details/202203243888660"})
-        except Exception as e:
-            print(f'error : {str(e)}')
-            
-        self.database.disconnect()
-        
-        print(listings)
-    
-    def getPendingListings(self):
-        listings = []
-        self.database.connect()
-        try:
-            listings = self.database.recCustomQuery("SELECT ID,registration,product_url FROM `fl_listings` WHERE number_plate_flag=1 AND Status='active' LIMIT 100")
-        except Exception as e:
-            print(f'error : {str(e)}')
-        self.database.disconnect()
-        return listings
-    
-    def getListingImages(self,listingId):
-        listingImages = []
-        self.database.connect()
-        try:
-            listingImages = self.database.recCustomQuery(f'SELECT Original FROM `fl_listing_photos` WHERE Listing_ID={listingId}')
-        except Exception as e:
-            print(f'error : {str(e)}')
-        self.database.disconnect()
-        return listingImages
     
     def validateRegistration(self,predicted,actual):
         regNo = str(predicted).strip().upper()
