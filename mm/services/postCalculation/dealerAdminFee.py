5271569f142cb8b81a7bd19f2e5a37060f55a21d
Yug Damor <yugdamor.dev@gmail.com>
2022-04-25 23:49:38 +0530

    done

--- mm/services/postCalculation/dealerAdminFee.py
+++ mm/services/postCalculation/dealerAdminFee.py
@@ -15,7 +15,7 @@ class dealerAdminFee:
             result = self.db.recCustomQuery(f'SELECT admin_fee FROM fl_dealer_scraper WHERE status="active" AND dealer_id={dealerId}')
             
             if len(result) > 0:
-                adminFee = int(result["admin_fee"])
+                adminFee = int(result[0]["admin_fee"])
         except Exception as e:
             print(f'error : {__file__} : {str(e)}')
         
