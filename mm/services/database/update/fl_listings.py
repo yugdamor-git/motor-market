23414ef8d2b83836e4a99bbfe3c438ef8368156e
Yug Damor <yugdamor.dev@gmail.com>
2022-05-17 16:18:31 +0530

    done

---
+++ mm/services/database/update/fl_listings.py
@@ -0,0 +1,99 @@
+from Database import Database
+
+from pulsar_manager import PulsarManager
+
+import traceback
+
+class topicHandler:
+    def __init__(self):
+        print("transform topic handler init")
+        
+        pulsar_manager = PulsarManager()
+        
+        self.topics = pulsar_manager.topics
+        
+        self.logs_producer = pulsar_manager.create_producer(pulsar_manager.topics.LOGS)
+        
+        self.fl_listings_update_consumer = pulsar_manager.create_consumer(pulsar_manager.topics.FL_LISTINGS_UPDATE)
+        
+        self.db = Database()
+        
+    def increase_expired_count(self):
+        self.logs_producer.produce_message(
+                {
+                    "eventType":"listingCount",
+                    "data":{
+                            "countFor":"expired"
+                        }
+                    }
+                )
+        
+    def handle_update_event(self,what,where):
+        
+        
+        what["updated_at"] = {"func":"now()"}
+       
+        if "Status" in what or "status" in what:
+            if what["Status"] == "expired":
+                self.increase_expired_count()
+        
+        self.db.connect()
+        
+        try:
+            self.db.recUpdate("fl_listings",what,where)
+        except Exception as e:
+            print(f'error : {str(e)}')
+        
+        self.db.disconnect()
+            
+    def main(self):
+        
+        while True:
+            try:
+                message =  self.fl_listings_update_consumer.consume_message()
+                
+                data = message.get("data",None)
+                
+                if data == None:
+                    print(f'this message does not contain data object.')
+                    print(f'message : {message}')
+                    continue
+                
+                event_type = data.get("event_type",None)
+                
+                print(f'event : {event_type}')
+                
+                if event_type != "update" or event_type == None:
+                    print(f'the event type({event_type}) is not update.skipping')
+                    continue
+                
+                source_url = data.get("sourceUrl",None)
+                
+                what = data.get("what",None)
+                where = data.get("where",None)
+                
+                if what == None or where == None:
+                    print(f'what and where is not present. skipping...')
+                    continue
+                
+                self.handle_update_event(what,where)
+                
+            except Exception as e:
+                print(f'error : {str(e)}')
+                
+                log = {}
+                
+                log["sourceUrl"] = source_url
+                
+                log["service"] = self.topics.FL_LISTINGS_UPDATE
+                
+                log["errorMessage"] = traceback.format_exc()
+                
+                self.logs_producer.produce_message({
+                    "eventType":"insertLog",
+                    "data":log
+                })
+
+if __name__ == "__main__":
+    th = topicHandler()
+    th.main()
---
+++ mm/services/database/update/pulsar_manager.py
@@ -0,0 +1,53 @@
+import pulsar
+import json
+from enum import Enum
+
+URI='pulsar://pulsar'
+
+class Topics(Enum):
+    LOGS = "motormarket.scraper.logs"
+    FL_LISTINGS_UPDATE = 'motormarket.database.fllistings.update'
+
+class Producer:
+    def __init__(self,producer_client) -> None:
+        self.producer_client = producer_client
+    
+    def produce_message(self,data):
+        
+        self.producer_client.send(
+            json.dumps(data).encode("utf-8")
+        )
+        
+class Parser:
+    def __init__(self) -> None:
+        pass
+    
+    def json_parser(self,data):
+        return json.loads(data)
+    
+
+class Consumer:
+    def __init__(self,consumer_client) -> None:
+        self.consumer_client = consumer_client
+        self.parser = Parser()
+    
+    def print(self,message):
+        print(message)
+    
+    def consume_message(self):
+        message = self.consumer_client.receive()
+        self.consumer_client.acknowledge(message)
+        self.print(f'message_id : {message.message_id()}')
+        return self.parser.json_parser(message)
+    
+class PulsarManager:
+    def __init__(self):
+        self.topics = Topics
+        self.uri =  URI
+        self.client = pulsar.Client(self.uri)
+        
+    def create_producer(self,topic:Topics):
+        return Producer(self.client.create_producer(topic))
+    
+    def create_consumer(self,topic:Topics):
+        return Consumer(self.client.subscribe(topic.value,f'{topic.name}-subscription',pulsar.ConsumerType.Shared))
