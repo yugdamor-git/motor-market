2280b541e8161d9adc64e4ec04bc7e7577dd8128
Yug Damor <yugdamor.dev@gmail.com>
2022-05-21 16:58:19 +0530

    done

--- mm/restart_scraping_system.py
+++ mm/restart_scraping_system.py
@@ -17,6 +17,11 @@ os.system(start_parent_command)
 
 print(f'parent services are up and running...')
 
+delay = 60
+for t in range(0,delay):
+    print(f'starting in {delay - t} seconds')
+
+
 # start child services
 start_child_services_command = "docker-compose --compatibility up -d logs fl-listings-find fl-listings-find-at-urls fl-listings-update fl-listings-update-at-urls at-urls-update fl-listings-insert fl-listings-insert-at-urls fl-listingphotos-insert fl-listingphotos-insert-at-urls listing-scraper listing-scraper-at-urls transform transform-at-urls pre-validation pre-validation-at-urls makemodel-prediction makemodel-prediction-at-urls seat-prediction seat-prediction-at-urls image-prediction image-prediction-at-urls post-validation post-validation-at-urls numberplate-prediction numberplate-prediction-at-urls post-calculation post-calculation-at-urls image-generation image-generation-at-urls at-urls-scraper"
 os.system(start_child_services_command)
