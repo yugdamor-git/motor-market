a629ce75a67d75a07c5049026887023625287a86
yug damor <yugdamor.dev@gmail.com>
2022-06-19 01:53:20 +0530

    Create database.py

---
+++ mm/services/api/src/database.py
@@ -0,0 +1,16 @@
+import os
+
+user = os.environ.get("MONGO_USERNAME")
+password = os.environ.get("MONGO_PASSWORD")
+host = "mongodb:27017"
+database = "motormarket-logs"
+
+import pymongo
+
+class Database:
+    def __init__(self):
+        db_name = database
+        connection_uri = f'mongodb://{user}:{password}@{host}/?authSource=admin'
+        client = pymongo.MongoClient(connection_uri)
+        db = client[db_name]
+        self.listing_count = db["listing-count"]
