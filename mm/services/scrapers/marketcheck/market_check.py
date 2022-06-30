14f520f5a8d9053715d31f771956a503bb68079e
speed-fusion <106366121+speed-fusion@users.noreply.github.com>
2022-06-30 11:54:24 +0530

    done

--- mm/services/scrapers/marketcheck/market_check.py
+++ mm/services/scrapers/marketcheck/market_check.py
@@ -55,53 +55,57 @@ class MarketCheck:
         listing_df.drop_duplicates(inplace=True)
         
         for index,row in listing_df.iterrows():
-            row_dict = row.to_dict()
-            tmp = {}
-            
-            tmp["sourceId"] = row_dict["id"]
-            tmp["sourceUrl"] = row_dict["vdp_url"]
-            tmp["price"] = row_dict["price"]
-            tmp["mileage"] = row_dict["miles"]
-            tmp["built"] = row_dict["year"]
-            tmp["make"] = row_dict["make"]
-            tmp["model"] = row_dict["model"]
-            tmp["trim"] = row_dict["variant"]
-            tmp["bodyStyle"] = row_dict["body_type"]
-            tmp["fuel"] = row_dict["fuel_type"]
-            tmp["transmission"] = row_dict["transmission"]
-            tmp["doors"] = row_dict["doors"]
-            tmp["registration"] = row_dict["vehicle_registration_mark"]
-            tmp["registration_date"] = row_dict["vehicle_registration_date"]
-            tmp["exterior_color"] = row_dict["exterior_color"]
-            tmp["dealer_id"] = row_dict["dealer_id"]
-            tmp["dealer_name"] = row_dict["seller_name"]
-            tmp["dealer_number"] = row_dict["seller_phone"]
-            
-            tmp["location_json"] = {
-                "street":row_dict["street"],
-                "city":row_dict["city"],
-                "county":row_dict["county"],
-                "postal_code":row_dict["postal_code"],
-                "country":row_dict["country"]
-            }
-            
-            tmp["dealer_location"] = row_dict["postal_code"]
-            tmp["images"] = []
-            if row_dict["photo_links"] != np.nan:
-                for i in row_dict["photo_links"].split("|"):
-                    if len(i) < 6:
-                        continue
-                    tmp["images"].append(i)
-            
-            tmp["engineCylinders"] = self.parse_engine_size(row_dict["engine_size"])
-            
-            tmp["accountId"] = self.accountId
-            tmp["websiteId"] = self.websiteId
-            tmp["featuredId"] = self.featureId
-            tmp["planId"] = self.planId
-            tmp["priority"] = self.priority
-            
-            listings.append(tmp)
+            try:
+                row_dict = row.to_dict()
+                tmp = {}
+                
+                tmp["sourceId"] = row_dict["id"]
+                tmp["sourceUrl"] = row_dict["vdp_url"]
+                tmp["price"] = row_dict["price"]
+                tmp["mileage"] = row_dict["miles"]
+                tmp["built"] = row_dict["year"]
+                tmp["make"] = row_dict["make"]
+                tmp["model"] = row_dict["model"]
+                tmp["trim"] = row_dict["variant"]
+                tmp["bodyStyle"] = row_dict["body_type"]
+                tmp["fuel"] = row_dict["fuel_type"]
+                tmp["transmission"] = row_dict["transmission"]
+                tmp["doors"] = row_dict["doors"]
+                tmp["registration"] = row_dict["vehicle_registration_mark"]
+                tmp["registration_date"] = row_dict["vehicle_registration_date"]
+                tmp["exterior_color"] = row_dict["exterior_color"]
+                tmp["dealer_id"] = row_dict["dealer_id"]
+                tmp["dealer_name"] = row_dict["seller_name"]
+                tmp["dealer_number"] = row_dict["seller_phone"]
+                
+                tmp["location_json"] = {
+                    "street":row_dict["street"],
+                    "city":row_dict["city"],
+                    "county":row_dict["county"],
+                    "postal_code":row_dict["postal_code"],
+                    "country":row_dict["country"]
+                }
+                
+                tmp["dealer_location"] = row_dict["postal_code"]
+                tmp["images"] = []
+                if type(row_dict["photo_links"]) == str:
+                    for i in row_dict["photo_links"].split("|"):
+                        if len(i) < 6:
+                            continue
+                        tmp["images"].append(i)
+                
+                tmp["engineCylinders"] = self.parse_engine_size(row_dict["engine_size"])
+                
+                tmp["accountId"] = self.accountId
+                tmp["websiteId"] = self.websiteId
+                tmp["featuredId"] = self.featureId
+                tmp["planId"] = self.planId
+                tmp["priority"] = self.priority
+                
+                listings.append(tmp)
+            except Exception as e:
+                print(f'error : {str(e)}')
+                print(row_dict)
             
         return listings
     
